f47df999bec7dc782ce56c3422331792695cf951
Christopher <christopher.oblak@gmail.com>
2022-03-13 11:27:55 -0400

    adding scripts

---
+++ scripts/data_PrepLabel.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import pyarrow as pa
+import os
+import json
+
+
+if __name__ == "__main__":
+    
+    print("Building Labeled Data....")
+    
+    # Adding Twitter Data Prep
+
+    twitter_columns = ['text', 'airline_sentiment']
+
+    twitter_df = pd.read_csv('data/raw_unformated/Tweets.csv', header=0, usecols=twitter_columns)
+    twitter_df.rename(columns={"airline_sentiment": "sentiment", "input": "text"}, inplace=True)
+
+    # Adding Rotten Tomato Data Prep
+
+    """
+    Citation:
+    Richard Socher, Alex Perelygin, Jean Wu, Jason Chuang, Christopher Manning, Andrew Ng and Christopher Potts
+    Conference on Empirical Methods in Natural Language Processing (EMNLP 2013)
+
+    General Information About the Data:
+    sentiment_labels.txt contains all phrase ids and the corresponding sentiment labels, separated by a vertical line.
+    Note that you can recover the 5 classes by mapping the positivity probability using the following cut-offs:
+    [0, 0.2], (0.2, 0.4], (0.4, 0.6], (0.6, 0.8], (0.8, 1.0]
+    for very negative, negative, neutral, positive, very positive, respectively.
+    Please note that phrase ids and sentence ids are not the same.
+    """
+
+    rot_df = pd.read_json('data/raw_unformated/reviews_Movies_and_TV_5.json.gz', lines=True, compression='gzip')
+
+    rot_df_final = pd.DataFrame()
+    rot_df_final["sentiment"] = rot_df["overall"]
+    rot_df_final["text"] = rot_df["reviewText"]
+    rot_df_final["sentiment"] = rot_df_final["sentiment"].replace(1, "negative")
+    rot_df_final["sentiment"] = rot_df_final["sentiment"].replace(3, "neutral")
+    rot_df_final["sentiment"] = rot_df_final["sentiment"].replace(5, "positive")
+
+    label_list = ["negative", "neutral", "positive"]
+
+    # keep only 1, 3, and 5 ratings
+    rot_df_final = rot_df_final[rot_df_final["sentiment"].isin(label_list)]
+
+    # Adding HuggyFace
+
+    """
+    Citation:
+    @InProceedings{Hamborg2021b,
+      author    = {Hamborg, Felix and Donnay, Karsten},
+      title     = {NewsMTSC: (Multi-)Target-dependent Sentiment Classification in News Articles},
+      booktitle = {Proceedings of the 16th Conference of the European Chapter of the Association for Computational Linguistics (EACL 2021)},
+      year      = {2021},
+      month     = {Apr.},
+      location  = {Virtual Event},
+    }
+    """
+    hf_1 = pd.read_json('data/raw_unformated/news_sentiment_newsmtsc/dev.jsonl', lines=True)
+    hf_2 = pd.read_json('data/raw_unformated/news_sentiment_newsmtsc/test.jsonl', lines=True)
+    hf_3 = pd.read_json('data/raw_unformated/news_sentiment_newsmtsc/train.jsonl', lines=True)
+
+    huggyface_df = pd.concat([hf_1, hf_2, hf_3])
+
+    huggyface_df = huggyface_df.drop_duplicates()
+
+    huggyface_df_final = pd.DataFrame()
+    huggyface_df_final["sentiment"] = huggyface_df['polarity']
+    huggyface_df_final["text"] = huggyface_df['sentence']
+    huggyface_df_final = huggyface_df_final.replace(-1, "negative")
+    huggyface_df_final = huggyface_df_final.replace(0, "neutral")
+    huggyface_df_final = huggyface_df_final.replace(1, "positive")
+
+    # Combine all datasets
+
+    final_df = pd.concat([twitter_df, rot_df_final, huggyface_df_final])
+
+    final_df.to_parquet("data/data_prepipeline/full_raw_data.parquet.gzip", compression='gzip')
+    
+    print("Updated Labeled Data Now Saved in data/data_prepipeline")
---
+++ scripts/data_preprocessing.py
@@ -0,0 +1,44 @@
+import string
+string.punctuation
+
+# defining the function to remove punctuation
+
+def remove_punctuation(text):
+    punctuationfree="".join([i for i in text if i not in string.punctuation])
+    return punctuationfree
+# storing the puntuation free text
+
+# defining function for tokenization
+
+import re
+
+def tokenization(text):
+    tokens = re.split('W+',text)
+    return tokens
+
+#importing nlp library
+import nltk
+nltk.download('omw-1.4')
+
+#Stop words present in the library
+stopwords = nltk.corpus.stopwords.words('english')
+def remove_stopwords(text):
+    output= [i for i in text if i not in stopwords]
+    return output
+
+from nltk.stem import WordNetLemmatizer
+#defining the object for Lemmatization
+wordnet_lemmatizer = WordNetLemmatizer()
+
+#defining the function for lemmatization
+def lemmatizer(text):
+    lemm_text = [wordnet_lemmatizer.lemmatize(word) for word in text]
+    return lemm_text
+
+def proprocessing(data):
+    data= data.apply(lambda x:remove_punctuation(x))
+    data= data.apply(lambda x: x.lower())
+    data= data.apply(lambda x: tokenization(x))
+    data= data.apply(lambda x:remove_stopwords(x))
+    data= data.apply(lambda x:lemmatizer(x))
+    return data
