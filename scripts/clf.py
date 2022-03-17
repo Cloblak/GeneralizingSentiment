53dc094c8febb2e65d68d7c8d5c3644ae894815f
Christopher Oblak <59293608+Cloblak@users.noreply.github.com>
2022-03-16 21:03:14 -0400

    change vocab load

--- scripts/clf.py
+++ scripts/clf.py
@@ -55,15 +55,7 @@ def read_vocab(path):
     return vocab
 
 def predict(text):
-#     labeled_df = pd.read_parquet("full_raw_data.parquet.gzip")
-#     labeled_df['sentiment'] = labeled_df['sentiment'].map({"neutral":1,"positive":2,"negative":0})
-#     train_df ,test_df = train_test_split(labeled_df,test_size=0.2)
-#     train_iter = [(label,text) for label,text in zip(train_df['sentiment'].to_list(),train_df['text'].to_list())]
-    
-    
-#     # Build vocabulary from tokens of training set
-#     tokenizer = get_tokenizer('basic_english')
-#     vocab = build_vocab_from_iterator(yield_tokens(train_iter,tokenizer), specials=["<unk>"])
+    vocab.load("vocab_obj.pth")
     vocab.set_default_index(vocab["<unk>"])
     vocab_size = len(vocab)
     print(vocab_size)
