a2583e357c04374415fd6803f10168bbc0eb279b
Cloblak <Christopher.Oblak@gmail.com>
2022-03-19 10:33:01 -0400

    changes to requirements and vocab build

--- scripts/vocabBuild.py
+++ scripts/vocabBuild.py
@@ -15,15 +15,15 @@ def yield_tokens(data_iter,tokenizer):
         yield tokenizer(text)
 
 if __name__ == "__main__":
-  print(os.getcwd())
-  labeled_df = pd.read_parquet("../data/data_prepipeline/full_raw_data.parquet.gzip")
-  labeled_df['sentiment'] = labeled_df['sentiment'].map({"neutral":1,"positive":2,"negative":0})
-  train_df ,test_df = train_test_split(labeled_df,test_size=0.2)
-  train_iter = [(label,text) for label,text in zip(train_df['sentiment'].to_list(),train_df['text'].to_list())]
+    print(os.getcwd())
+    labeled_df = pd.read_parquet("../data/data_prepipeline/full_raw_data.parquet.gzip")
+    labeled_df['sentiment'] = labeled_df['sentiment'].map({"neutral":1,"positive":2,"negative":0})
+    train_df ,test_df = train_test_split(labeled_df,test_size=0.2)
+    train_iter = [(label,text) for label,text in zip(train_df['sentiment'].to_list(),train_df['text'].to_list())]
   
   
-  # Build vocabulary from tokens of training set
-  tokenizer = get_tokenizer('basic_english')
-  vocab = build_vocab_from_iterator(yield_tokens(train_iter,tokenizer), specials=["<unk>"])
-  
-  torch.save(vocab, "vocab_obj.pth")
+    # Build vocabulary from tokens of training set
+    tokenizer = get_tokenizer('basic_english')
+    vocab = build_vocab_from_iterator(yield_tokens(train_iter,tokenizer), specials=["<unk>"])
+
+    torch.save(vocab, "vocab_obj.pth")
