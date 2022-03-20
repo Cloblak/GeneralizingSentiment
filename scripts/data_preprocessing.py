1517ad904f0d5ec01b7003300d6137c19d22be94
Liu Yudong <87921304+1999Lyd@users.noreply.github.com>
2022-03-21 06:29:52 +0800

    Update data_preprocessing.py

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -43,6 +43,5 @@ def preprocessing(data):
     data= data.apply(lambda x: tokenization(x))
     data= data.apply(lambda x:remove_stopwords(x))
     data= data.apply(lambda x:lemmatizer(x))
-    tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3), min_df=0, stop_words='english')
-    tfidf_matrix = tf.fit_transform(data)
-    return data,tfidf_matrix
+    
+    return data
